8ddaab6727afaacdde9326a0604c9f0bb36be626
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-04-24 19:52:28 +0100

    styling the answer button

--- challenge/lib/core/views/home/home_view.dart
+++ challenge/lib/core/views/home/home_view.dart
@@ -12,7 +12,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final questionHomeStore = QuestionHomeStore();
+  final _questionHomeStore = QuestionHomeStore();
   final _questions = const [
     {
       'question': 'You have a Dog ?',
@@ -22,77 +22,118 @@ class _HomeViewState extends State<HomeView> {
       ],
     },
     {
-      'question': 'You have a Dogi ?',
+      'question': 'First Computer Invented ?',
       'answers': [
-        {'answer': 'WOU'},
-        {'answer': 'wAA'}
+        {'answer': 'Obacus'},
+        {'answer': 'Anilitc Machine'},
+        {'answer': 'Pascaline'}
       ],
     },
   ];
-  // void _nextQuestion() {
-  //   setState(() {
-  //     _questionIndex += 1;
-  //   });
-  // }
 
-  // void _previousQuestion() {
-  //   setState(() {
-  //     _questionIndex -= 1;
-  //   });
-  // }
-
-  // void _playAgain(int value) {
-  //   setState(() {
-  //     _questionIndex = value;
-  //   });
-  // }
+  Widget showAsnwers(String answer) {
+    if (_questionHomeStore.questionIndex == 0) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(30)),
+            child: TextButton(
+              child: Text(answer),
+              onPressed: _questionHomeStore.nextQuestion,
+            ),
+          ),
+        ],
+      );
+    } else if (_questionHomeStore.questionIndex == 1) {
+      return TextButton(
+        child: Text('1' + answer),
+        onPressed: _questionHomeStore.nextQuestion,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Observer(builder: (context) {
-      return questionHomeStore.questionIndex < _questions.length
-          ? Column(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(10, 35, 20, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      questionHomeStore.questionIndex >= 1
-                          ? CustomButtonPrevious(
-                              questionHomeStore.previousQuestion)
-                          : Container(),
-                      CustomSizedBox(widht: 20),
-                      CustomButtonNext(questionHomeStore.nextQuestion),
-                    ],
-                  ),
+      return _questionHomeStore.questionIndex < _questions.length
+          ? Column(children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 35, 20, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    _questionHomeStore.questionIndex >= 1
+                        ? CustomButtonPrevious(
+                            _questionHomeStore.previousQuestion)
+                        : Container(),
+                    CustomSizedBox(widht: 20),
+                    CustomButtonNext(_questionHomeStore.nextQuestion),
+                  ],
                 ),
-                Expanded(
+              ),
+              Expanded(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      CustomQuestionTitle(
-                          _questions[questionHomeStore.questionIndex]
-                              ['question']),
-                      ...(_questions[questionHomeStore.questionIndex]['answers']
-                              as List<Map<String, Object>>)
-                          .map((answer) {
-                        return TextButton(
-                          child: Text(answer['answer']),
-                          onPressed: questionHomeStore.nextQuestion,
-                        );
-                      }).toList(),
-                    ],
-                  ),
-                ),
-              ],
-            )
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    CustomQuestionTitle(
+                        _questions[_questionHomeStore.questionIndex]
+                            ['question']),
+                    _questionHomeStore.questionIndex == 0
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ...(_questions[_questionHomeStore.questionIndex]
+                                      ['answers'] as List<Map<String, Object>>)
+                                  .map((answer) {
+                                return Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade100,
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: TextButton(
+                                    child: Text(
+                                      answer['answer'],
+                                      style: TextStyle(fontSize: 30),
+                                    ),
+                                    onPressed: _questionHomeStore.nextQuestion,
+                                  ),
+                                );
+                              }).toList(),
+                            ],
+                          )
+                        : Container(
+                            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ...(_questions[_questionHomeStore
+                                              .questionIndex]['answers']
+                                          as List<Map<String, Object>>)
+                                      .map(
+                                    (answer) {
+                                      return TextButton(
+                                        child: Text(answer['answer']),
+                                        onPressed:
+                                            _questionHomeStore.nextQuestion,
+                                      );
+                                    },
+                                  ),
+                                ]))
+                  ]))
+            ])
           : Center(
               child: TextButton(
                   child: Text('Start Again'),
-                  onPressed: () => questionHomeStore.playAgain()),
-            );
+                  onPressed: () => _questionHomeStore.playAgain()));
     }));
   }
 }
